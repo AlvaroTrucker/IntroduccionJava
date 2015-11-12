57c920150fda1eb6dfdf67651499896cded0dd67
manolo molino <manolo@iesvirgendelcarmen.com>
2015-11-12 11:56:25 +0100

    Creando repositorio del tema 2

---
+++ HolaMundoDoc.java
@@ -0,0 +1,21 @@
+//Fichero de la HolaMundo
+
+//esto es un comentario de línea
+
+/*También existen comentarios de bloque
+que nos sirven para comentar mas
+de una línea. Lo que no hay que olvidar
+es cerrar el comentario */
+
+//existe la posibilidad de generar documentación
+/** <em>Clase HolaMundo</em>
+que solo va a mostrar en pantalla el 
+típico Hello World
+@author Manuel Molino
+@version 1.0
+*/
+public class HolaMundoDoc{
+	public static void main(String[] args){ //método main
+		System.out.println("Hola mundo");
+	}
+}
---
+++ NombreEdad.java
@@ -0,0 +1,9 @@
+public class NombreEdad{
+	public static void main(String[] args){
+		System.out.println("Federico Martínez García 35");
+		System.out.println("Federico Martínez García 35");
+		System.out.println("Federico Martínez García 35");
+		System.out.println("Federico Martínez García 35");
+		System.out.println("Federico Martínez García 35");
+	}
+}
